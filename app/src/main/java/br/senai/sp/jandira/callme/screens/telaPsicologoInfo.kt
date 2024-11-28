381bdbfc23dcc06c8bf2d944460a2b0e5a1c74e1
Giovanna Belo <123781727+giovbel@users.noreply.github.com>
2024-11-28 16:58:29 -0300

    Add files via upload

---
+++ app/src/main/java/br/senai/sp/jandira/callme/screens/telaPsicologoInfo.kt
@@ -0,0 +1,9 @@
+package br.senai.sp.jandira.callme.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun telaPsicologoInfo(controleNavegacao: NavHostController) {
+
+}
